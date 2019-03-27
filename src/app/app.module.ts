1bb0313267d59b54333917bf3f0f4811b6882add
neni@zuhlke.com <7CAFF123zuhlke>
2019-03-27 20:39:59 +0100

    Simple layout and basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { SidebarNavigationComponent } from './sidebar-navigation/sidebar-navigation.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    SidebarNavigationComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidebar-navigation/sidebar-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sidebar-navigation',
+  templateUrl: './sidebar-navigation.component.html',
+  styleUrls: ['./sidebar-navigation.component.css']
+})
+export class SidebarNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

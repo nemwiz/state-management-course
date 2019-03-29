c16027a3f9cb316b3014da787b6317dbf2bfdd5b
Ognjen Kurtic <Ognjen.Kurtic@zuehlke.com>
2019-03-29 12:30:28 +0100

    Add shoping cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from 'src/app/product-details/product-details.component';
 import { ProductComponent } from 'src/app/products/product/product.component';
 import { ProductsComponent } from 'src/app/products/products.component';
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { ShoppingCartComponent } from './shooping-cart/shooping-cart.component';
 import { SidebarNavigationComponent } from './sidebar-navigation/sidebar-navigation.component';
 
 
 const routes: Routes = [
-  { path: '', component: ProductsComponent, pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetailsComponent }
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: ShoppingCartComponent },
 ];
 
 @NgModule({
@@ -22,7 +24,8 @@ const routes: Routes = [
     SidebarNavigationComponent,
     ProductsComponent,
     ProductComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shooping-cart/shooping-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shooping-cart',
+  templateUrl: './shooping-cart.component.html',
+  styleUrls: ['./shooping-cart.component.css']
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

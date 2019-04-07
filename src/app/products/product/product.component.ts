4f4210af7e6f29ee04f972974d9d30931397a801
neni@zuhlke.com <7CAFF123zuhlke>
2019-04-07 17:11:30 +0200

    Improved sidebar navigation

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Product} from '../../models/product';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'product',
@@ -10,6 +11,17 @@ export class ProductComponent implements OnInit {
 
   @Input() product: Product;
 
+  constructor(private router: Router) {
+  }
+
   ngOnInit(): void {
+  }
+
+  navigateToProductDetails() {
+    this.router.navigateByUrl(`products/${this.product.id}`);
+  }
+
+  addToCart() {
+    // TODO - implement logic for cart state
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Product} from '../models/product';
-import {Router} from '@angular/router';
 import {ProductService} from '../services/product.service';
 
 @Component({
@@ -12,15 +11,12 @@ export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
 
-  constructor(private router: Router, private productsService: ProductService) {
+  constructor(private productsService: ProductService) {
   }
 
   ngOnInit() {
     this.products = this.productsService.getProducts();
   }
 
-  navigateToProductDetails(productId: number) {
-    this.router.navigateByUrl(`products/${productId}`);
-  }
 
 }

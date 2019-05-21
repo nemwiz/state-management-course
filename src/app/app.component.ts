6322f3e9f47344cf907d8c8c3c084e7892e72718
neni@zuhlke.com <7CAFF123zuhlke>
2019-05-21 11:10:48 +0200

    Added comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'state-management-course';
+
+  ngOnInit(): void {
+    // add all notifications to store
+  }
 }
